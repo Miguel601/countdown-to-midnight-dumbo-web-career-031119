d98cc63198943a49fcf15444e143e49e26f66845
Miguel Henriquez <henriquez.miguel@outlook.com>
2019-02-28 19:27:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
-#write your code here
 
-def countdown
+# fast countdown
+def countdown(i)
+  while i > 0
+    puts "#{i} SECOND(S)!"
+
+    i -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+# suspensful countdown
+def countdown_with_sleep(i)
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    sleep(1)
+
+    i -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
